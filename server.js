fa087f79b05d2f9d8ec8e8668ab411ef3f0ef483
Rapalli Shesaeasha <137075837+RapalliShesaeasha@users.noreply.github.com>
2024-09-04 17:09:57 +0530

    Update server.js

--- server.js
+++ server.js
@@ -19,6 +19,11 @@ app.use(cors());
 // Routes
 app.use('/api/auth', authRoutes);
 
+// Test Route
+app.get('/api/test', (req, res) => {
+  res.status(200).json({ message: 'The server is running and this is a test route!' });
+});
+
 // Start the server
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
